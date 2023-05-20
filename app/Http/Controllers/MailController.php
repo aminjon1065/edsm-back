1aae068f884d037957e7fbfba46412cb2927b705
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2023-05-20 11:10:36 +0500

    0.0.1

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -9,9 +9,16 @@ class MailController extends Controller
 {
     public function sendMail(Request $request)
     {
-        dd(json_encode($request->input('to')));
-//        dd($request->to[0]);
-        foreach ($request['to'] as $to) {
+//        dd(json_encode($request->input('to')));
+
+        $toArr[] = $request['to'];
+        $toArrs = [...$toArr[0]];
+//        return response()->json($toArrs);
+//        if (!empty($toArr) && isset($toArr) && is_array($toArr)) {
+        foreach ($toArrs as $key => $item) {
+            $sendTo[$key] = $item;
+        }
+        foreach ($sendTo as $key => $to) {
             Mail::create([
                 'to' => $to,
                 'from' => auth()->user()->id,
